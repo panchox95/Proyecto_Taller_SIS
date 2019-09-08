8b6ac7b05b44fe23b647a4ff5f8a9aa76309fc7a
Vladimir Romecin <romecin.duarte.diego@gmail.com>
2019-09-08 19:35:34 -0400

    plantilla de login y register angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  addEmail(email){
+    console.log(email.value);
+    email.value='';
+    return 'app-register';
+  }
 }
