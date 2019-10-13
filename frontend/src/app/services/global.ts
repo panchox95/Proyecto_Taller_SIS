a038b6297a10a2e1e58320c98b75d9324b7d88ab
unknown <jeffcristhlb@gmail.com>
2019-10-13 17:42:24 -0400

    url jeff

--- frontend/src/app/services/global.ts
+++ frontend/src/app/services/global.ts
@@ -1,10 +1,13 @@
 export var GLOBAL={
     //CHICOS NO TOQUEN LAS RUTAS AQUI, SI NO FUNCIONA ES PORQUE LA QUE USTEDES ESTAN USANDO NO ESTA ACTIVA
     //COMENTEN!!! LAS OTRAS PARA NO PERJUDICAR A LOS OTROS
-    
-    
-    
+
+
+
     // url: 'http://127.0.0.1:8000/api/'
     //url: 'http://localhost:8888/Proyecto_Taller_SIS/api/'
-    url: 'http://tallersis.com.devel/api/'
+    //url: 'http://tallersis.com.devel/api/'
+    url: 'http://localhost/Proyecto_Taller_SIS/Backend_Sis/public/api/'
+
+
 };
