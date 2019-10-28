72481a3bd529dabb362fbd5904dcf64a4c5bf0bb
Francisco <Steverlynck>
2019-10-28 10:55:19 -0400

    Bien gil soy

--- Backend_Sis/app/Http/BL/OfertasBL.php
+++ Backend_Sis/app/Http/BL/OfertasBL.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\Http\BL;
 use Illuminate\Database\Eloquent\Model;
-use App\Oferta;
+use App\OfertaProducto;
 use App\Helpers\JwtAuth;
 class OfertasBL
 {
     public function crearOferta($params,$id_producto){
-        $oferta = new Oferta;
+        $oferta = new OfertaProducto;
         $oferta->saveOferta($params,$id_producto);
         return array('status'=>'SUCCESS',
             'code'=>200,
@@ -14,24 +14,24 @@ class OfertasBL
         );
     }
     public function borrarOferta($id_producto){
-        $oferta = new Oferta;
+        $oferta = new OfertaProducto;
         $oferta->eliminarOferta($id_producto);
         return array('status' => 'SUCCESS','message'=>'Producto Eliminado');
     }
     public function modificarOferta($params,$id){
-        $oferta = new Oferta;
+        $oferta = new OfertaProducto;
         $oferta = $oferta->modificarOferta($params,$id);
         $data = array('status' => 'SUCCESS','message'=>'Modificacion Exitosa');
         return $data;
     }
     public function verOferta($id){
-        $oferta = new Oferta;
+        $oferta = new OfertaProducto;
         $data =  array('status' => 'SUCCESS','message'=>'Oferta '.$id,'data'=>$oferta->verOferta($id));
 
         return $data;
     }
     public function listaOferta(){
-        $oferta = new Oferta;
+        $oferta = new OfertaProducto;
         $lista = $oferta->listaOferta();
         if(\is_object($lista)){
             $data = array('status' => 'SUCCESS','message'=>'lista de ofertas','ofertas'=>$lista);
