47058d4ebf29dddde241da66c278d765764c2eb0
unknown <jeffcristhlb@gmail.com>
2019-11-12 20:24:46 -0400

    Test Ofertas

--- Backend_Sis/tests/Feature/PromocionesTest.php
+++ Backend_Sis/tests/Feature/PromocionesTest.php
@@ -85,4 +85,151 @@ class PromocionesTest extends TestCase
             'status' => "SUCCESS"
         ]);
     }
+
+
+
+
+    public function testEliminarOfertaServicioComoUsuario()
+    {
+        $jwt = new JwtAuth();
+        $token1 = $jwt->getTokenUser();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/borraroferta/3');
+        $response->assertStatus(500);
+
+    }
+
+    public function testEliminarOfertaServicioSinSerUsuarioAuth()
+    {
+        $jwt = new JwtAuth();
+        $token1 = '';
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/borraroferta/3');
+        $response->assertStatus(403);
+
+
+
+    }
+
+    public function testOfertaEliminarServicioSinID()
+    {
+        $jwt = new JwtAuth();
+        $token1 = $jwt->getTokenUser();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/borraroferta/');
+        $response->assertStatus(404);
+    }
+
+    public function testModificarOfertaServicioAdmi()
+    {
+        $jwt = new JwtAuth();
+        $token1 = $jwt->getTokenAdmi();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/modificaroferta/3',[
+            "descripcion"=>"Oferta Modificada",
+            "descuento"=>50
+
+        ]);
+        $response->assertStatus(200);
+        $response->assertJson(['status'=>'SUCCESS']);
+        $response->assertJson(['message'=>'Modificacion Exitosa']);
+
+    }
+
+    public function testOfertaModificarServicioUserNormal()
+    {
+        $jwt = new JwtAuth();
+        $token1 = $jwt->getTokenUser();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/modificaroferta/3',[
+            "descripcion"=>"Oferta Modificada",
+            "descuento"=>50
+
+        ]);
+        $response->assertStatus(500);
+
+
+
+    }
+
+    public function testOfertaModificarServicioSinLogear()
+    {
+        $jwt = new JwtAuth();
+        $token1 = '';
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/modificaroferta/3',[
+            "descripcion"=>"Oferta Modificada",
+            "descuento"=>50
+
+        ]);
+        $response->assertStatus(403);
+        $response->assertJson(['status'=>'ERROR']);
+        $response->assertJson(['code'=>'400']);
+        $response->assertJson(['message'=>'Token Invalido']);
+
+
+    }
+
+    public function testOfertaModificarServicioSinEspecificar()
+    {
+        $jwt = new JwtAuth();
+        $token1 =  $jwt->getTokenAdmi();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('PUT','/api/modificaroferta/',[
+            "descripcion"=>"Oferta Modificada",
+            "descuento"=>50
+
+        ]);
+        $response->assertStatus(404);
+
+
+
+    }
+
+
+
+
+    public function testVerOfertaServicioEspecifico()
+    {
+        $jwt = new JwtAuth();
+        $token1 =  $jwt->getTokenAdmi();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('GET','/api/veroferta/3',[
+
+
+        ]);
+        $response->assertStatus(200);
+        $response->assertJson(['status'=>'SUCCESS']);
+
+    }
+
+    public function testVerOfertaServicioEspecificoInexistente()
+    {
+        $jwt = new JwtAuth();
+        $token1 =  $jwt->getTokenAdmi();
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization'=>$token1,
+        ])->json('GET','/api/veroferta/3546654',[
+
+
+        ]);
+        $response->assertStatus(500);
+    }
 }
