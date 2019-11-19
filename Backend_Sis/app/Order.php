8f0c8de0d23872ca1dfc8142cefcc742fe5d3f57
Juan Molina <molinajuanpablo1@gmail.com>
2019-11-19 18:33:52 -0400

    order user

--- Backend_Sis/app/Order.php
+++ Backend_Sis/app/Order.php
@@ -9,7 +9,5 @@ class Order extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
-    public function orders() {
-        return $this->hasMany('App\Order');
-    }
+
 }
