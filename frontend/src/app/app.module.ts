3588e04922d747911ec8340b900fc7cc6f9df8a7
Juan Molina <molinajuanpablo1@gmail.com>
2019-11-18 22:26:07 -0400

    login face gmail

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -43,7 +43,7 @@ export function getAuthServiceConfigs() {
     [
       {
         id: FacebookLoginProvider.PROVIDER_ID,
-        provider: new FacebookLoginProvider("Your-Facebook-app-id")
+        provider: new FacebookLoginProvider("581488765929996")
       },
       {
         id: GoogleLoginProvider.PROVIDER_ID,
