fa6176cb4df2167973a2b83018e678d14a408890
Juan Molina <molinajuanpablo1@gmail.com>
2019-12-11 00:04:59 -0400

    corregido un error

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -65,4 +65,6 @@ export class AppComponent implements OnInit, DoCheck{
 
   }
 
+
+
 }
--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -39,6 +39,7 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit() {
     this.getCheckout();
+    this.loadStripe();
   }
 
   getCheckout(){
@@ -56,20 +57,52 @@ export class CheckoutComponent implements OnInit {
     );
   }
 
-  postCheckout(CheckoutForm: NgForm){
-    let id_usuario: string;
-    const datos=jwt_decode(localStorage.getItem('token'));
-    id_usuario = datos.id_user;
-    console.log('estado: ', this.status_compra);
-    this._userService.postCheckout( request).subscribe(
-      response => {
-        console.log('Total a Pagar: ', response);
-        this.PrecioTotal=response;
-      },
-      error => {
-        console.log(<any>error);
+  // postCheckout(CheckoutForm: NgForm){
+  //   let id_usuario: string;
+  //   const datos=jwt_decode(localStorage.getItem('token'));
+  //   id_usuario = datos.id_user;
+  //   console.log('estado: ', this.status_compra);
+  //   this._userService.postCheckout( request).subscribe(
+  //     response => {
+  //       console.log('Total a Pagar: ', response);
+  //       this.PrecioTotal=response;
+  //     },
+  //     error => {
+  //       console.log(<any>error);
+  //     }
+  //   );
+  // }
+
+  loadStripe() {
+
+    if(!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(s);
+    }
+  }
+
+  pay(amount) {
+
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
       }
-    );
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100
+    });
+
   }
 
 }
